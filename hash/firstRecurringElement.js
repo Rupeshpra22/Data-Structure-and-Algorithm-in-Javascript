df5c3a9b0a632e80aeb6f6ff2059eb5bcaf60a10
Rupesh Prajapati <39094963+Rupeshpra22@users.noreply.github.com>
2021-07-05 20:22:33 +0530

    hash in js

--- hash/firstRecurringElement.js
+++ hash/firstRecurringElement.js
@@ -4,17 +4,26 @@ let arr = [2, 5, 1, 2, 3, 5, 1, 2, 4]
 let arr1 = [2, 1, 1, 2, 3, 5, 1, 2, 4]
 // output = 1
 
+let arr2 = [2, 5, 5, 2, 3, 5, 1, 2, 4]
+//output = 2
 
 let firstRecurringElement = (arr) => {
     let map = {};
-    for (let i = 0; i < arr.length; i++) {
-        if (map[arr[i]] !== undefined) {
-            return arr[i];
-        } else {
-            map[arr[i]] = i
+    for(let i = 0;i<arr.length;i++){
+        if(map[arr[i]]!== undefined){
+            map[arr[i]] += 1;
+        }else{
+            map[arr[i]] = 1
         }
     }
-    return undefined;
+    return map
 }
 
-console.log(firstRecurringElement(arr))
+let elementCount = firstRecurringElement(arr1)
+for(let i=0;i<arr1.length;i++){
+    let num = arr1[i];
+    if(elementCount[num]>1){
+        console.log(num)
+        return;
+    }
+}
